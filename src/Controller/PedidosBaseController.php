7f1518f6c70b0cdca4a2c2555975ff744747d63b
issa23i <ipaslop262@iesmartinezm.es>
2023-03-03 00:07:09 +0100

    estable configurando email

--- src/Controller/PedidosBaseController.php
+++ src/Controller/PedidosBaseController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,40 +14,43 @@ use App\Service\CestaCompra;
 use App\Entity\Usuario;
 use App\Entity\Pedido;
 use App\Entity\PedidosProductos;
+
 /**
-*@IsGranted("ROLE_USER")
-*/
-class PedidosBaseController extends AbstractController
-{
+ * @IsGranted("ROLE_USER")
+ */
+class PedidosBaseController extends AbstractController {
+
     /**
-     * Hace una consulta a la BD mediante en entity manager
+     * Hace una consulta a la BD mediante Manager Registry inyectado
      * obtiendo el objeto Familias
      * Con findAll se obtienen todas 
      * finalmente se renderiza en la vista
-     * inyectamos un servicio Manager Resgistry
      * @Route("/familias", name="familias")
      */
-    public function obtenerFamilias(ManagerRegistry $doctrine): Response
-    {
+    public function obtenerFamilias(ManagerRegistry $doctrine): Response {
         $familias = $doctrine->getRepository(Familia::class)->findAll();
         return $this->render('familia.html.twig', [
-            'familias' => $familias,
+                    'familias' => $familias,
         ]);
     }
-    
+
     /**
-     * inyectamos un servicio Manager Resgistry
+     * inyectamos un servicio Manager Resgistry,
+     * recoge el id de la familia del path 
+     * y con el id se usa doctrine para acceder a la BD 
+     * para obtener la familia y así poder conseguir sus productos
+     * accediendo a la entidad Familia - getProductos
+     * gracias a la relación oneToMany una familia muchos productos
      * @Route("/productos/{id_familia}", name="productos")
      */
-    public function obtenerProducts(ManagerRegistry $doctrine, $id_familia): Response
-    {
-        
+    public function obtenerProducts(ManagerRegistry $doctrine, $id_familia): Response {
+
         $productos = $doctrine->getRepository(Familia::class)->find($id_familia)->getProducts();
         return $this->render('listado_productos.html.twig', [
-            'productos' => $productos,
+                    'productos' => $productos,
         ]);
     }
-    
+
     /**
      * Recoge los datos del formulario, id, unidades y producto (request)
      *      y hace una consulta a la bbdd (doctrine)
@@ -56,19 +60,18 @@ class PedidosBaseController extends AbstractController
      * 
      * @Route("/anadir/{producto_id}", name="anadir")
      */
-    public function anadir(Request $request, ManagerRegistry $doctrine, $producto_id, CestaCompra $cesta ): Response {
-        
+    public function anadir(Request $request, ManagerRegistry $doctrine, $producto_id, CestaCompra $cesta): Response {
+
         $producto = $doctrine->getRepository(Product::class)->find($producto_id);
-        $unidades = floatval($request->request->get('unidades'));
-        
+        $unidades = intval($request->request->get('unidades'));
+
         // uso del servicio CestaCompra
-        $cesta->cambiar_unidades($producto,$unidades);
+        $cesta->cambiar_unidades($producto, $unidades);
         $cesta->guardarCesta();
-         // $this->addFlash('exito', 'Se ha añadido el producto');
-         //echo 'estoy en la función añadir';
+        
         return $this->redirectToRoute('cesta');
     }
-    
+
     /**
      * Recoge los datos del formulario de eliminar producto (request)
      * Hace una consulta a la bbdd con el entity manager doctrine
@@ -78,18 +81,18 @@ class PedidosBaseController extends AbstractController
      * 
      * @Route("/eliminar/{producto_id}", name="eliminar")
      */
-    public function eliminar(Request $request, ManagerRegistry $doctrine, $producto_id, CestaCompra $cesta ): Response {
-        
+    public function eliminar(Request $request, ManagerRegistry $doctrine, $producto_id, CestaCompra $cesta): Response {
+
         $producto = $doctrine->getRepository(Product::class)->find($producto_id);
         $unidades = floatval($request->request->get('unidades'));
-        
+
         // uso del servicio CestaCompra
         $cesta->eliminar($producto, $unidades);
         $cesta->guardarCesta();
-        
+
         return $this->redirectToRoute('cesta');
     }
-    
+
     /**
      * El controlador cesta recoge los datos de la sesión 
      *      , la obtiene del servicio CestaCompra
@@ -100,16 +103,17 @@ class PedidosBaseController extends AbstractController
      * @Route("/cesta", name="cesta")
      */
     public function obtenerCesta(CestaCompra $cesta): Response {
-        
+
         $carrito = $cesta->getCarrito();
         $total = $cesta->getTotal();
-        
+
         return $this->render('ver_cesta.html.twig'
-                                ,['carrito'=>$carrito, 'total'=>$total]);
+                        , ['carrito' => $carrito, 'total' => $total]);
     }
-    
+
     /**
-     * Obtiene el total con el request y el id del usuario 
+     * Obtiene el total con el request 
+     * y el id del usuario gracias a la interfaz UserInterface
      * Crea un nuevo Pedido y en el Entity manager, persiste y 
      * actualiza la base de datos, renderiza tanto si da error
      * como si se realiza con éxito
@@ -118,87 +122,77 @@ class PedidosBaseController extends AbstractController
      * @return Response
      * @Route("/pedido", name="pedido")
      */
-    public function pedido(Request $request, ManagerRegistry $doctrine, CestaCompra $cesta ): Response {
+    public function pedido(Request $request, ManagerRegistry $doctrine, CestaCompra $cesta): Response {
         $em = $doctrine->getManager();
-        
+
         $total = floatval($request->request->get('total'));
         $usuario = $this->getUser();
-        
+
         $pedido = new Pedido;
-        
+
         $pedido->setFecha(\DateTime::createFromFormat('Y-m-d', date("Y-m-d")));
         $pedido->setCoste($total);
         $pedido->setUsuario($usuario);
-        $cod_pedido;
-        
-        
-        try {
-            // guardar o persistir el producto
-            $em->persist($pedido);
-            // enviar la consulta a la bbdd 
-            $em->flush();
-            // sólo después de enviar el flush se puede 
-            // obtener el id (autoincremento)
-            $cod_pedido = $pedido->getId();
-            
-        } catch (Exception $exc) {
-            // si hubo error, obtiene el código del error 
-            // y retorna el renderizado con el código del error
-            // y el código de pedido será null
-            $codigo_error = $exc->getCode();
-            return $this->render('pedido.html.twig', [
-                'cod_pedido'=>null,
-                'error'=>$codigo_error,
-            ]);
-        }
-        /**
+
+        // guardar o persistir el producto
+        $em->persist($pedido);
+
         // persistir y flush de la tabla pedidos_productos
         $carrito = $cesta->getCarrito();
+        foreach ($carrito as $cod => $value) {
+            $pedido_producto = new PedidosProductos;
+            $id = $value['producto']->getId();
+            $producto = $doctrine->getRepository(Product::class)->find($id);
+            $pedido_producto->setProducto($producto);
+            $pedido_producto->setPedido($pedido);
+            $unidades = intval($value['unidades']);
+            $pedido_producto->setUnidades($unidades);
+            $em->persist($pedido_producto);
+        }
         try {
-            
-            foreach ($carrito as $producto) {
-                $pedido_producto = new PedidosProductos;
-                $pedido_producto->setProducto($producto['producto']);
-                $pedido_producto->setPedido($pedido);
-                $pedido_producto->setUnidades($producto['unidades']);
-                $em->persist($pedido_producto);
-            }
-            
+            // enviar la consulta a la bbdd 
             $em->flush();
         } catch (Exception $exc) {
             // si hubo error, obtiene el código del error 
-            // pone el pedido a null
             // y retorna el renderizado con el código del error
             // y el código de pedido será null
             $codigo_error = $exc->getCode();
-            $pedido->removePedidosProducto($pedido_producto);
             return $this->render('pedido.html.twig', [
-                'cod_pedido'=>null,
-                'error'=>$codigo_error,
+                        'cod_pedido' => null,
+                        'error' => $codigo_error,
             ]);
         }
-         * */
+
         
+        // obtener el id (autoincremento)
+        $cod_pedido = $pedido->getId();
+
         $cesta->borrarCesta();
         $cesta->guardarCesta();
-        
+
         // si no hay error, se pone a null el error
         return $this->render('pedido.html.twig', [
-            'cod_pedido'=>$cod_pedido,
-            'error'=> null,
+                    'cod_pedido' => $cod_pedido,
+                    'error' => null,
         ]);
     }
-    
+
     /**
-     * 
+     * La plantilla pedidos.html.twig mostrará todos los pedidos 
+     * de un usuario (código, fecha y coste total)
+     * , incluyendo una tabla por cada pedido con 
+     * los productos (código, nombre corto, precio) 
+     * y las unidades solicitadas.
      * @param CestaCompra $cesta
      * @Route("/pedidos", name="pedidos")
      */
-    public function pedidos(CestaCompra $cesta, ManagerRegistry $doctrine): Response {
-        $pedidos_productos = $doctrine->getRepository(PedidosProductos::class)->findAll();
+    public function pedidos(CestaCompra $cesta): Response {
+        $usuario = $this->getUser();
+        $pedidos = $usuario->getPedidos();
+        
         return $this->render('pedidos.html.twig', [
-            'pedidos_productos' => $pedidos_productos,
+                    'pedidos' => $pedidos,
         ]);
     }
-    
+
 }
